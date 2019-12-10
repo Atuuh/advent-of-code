ad08fc580ce863d34b2d24cb371cf4c9db133c7c
Atuuh <not-another-one@hotmail.com>
2019-12-10 07:37:47 +0000

    started day 10

---
+++ src/2019/day-10/day-10.test.ts
@@ -0,0 +1,5 @@
+describe('2019 - Day Ten', () => {
+    describe('Name of the group', () => {
+        test('should ', () => {});
+    });
+});
---
+++ src/2019/day-10/day-10.ts
@@ -0,0 +1,14 @@
+import { Day } from '#models/solution';
+
+export const DayTen: Day = () => {
+    const partOne = 0;
+    const partTwo = 0;
+
+    return { partOne, partTwo };
+};
+
+export const findSuitableAsteroid = (
+    map: Map<[number, number], boolean>
+): [number, number] => {
+    return [0, 0];
+};
