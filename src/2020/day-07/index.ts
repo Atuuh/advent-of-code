02ce66326ff8cde5415e027dcb6742a537dc7a71
Atuuh <not-another-one@hotmail.com>
2021-10-22 00:31:34 +0100

    Added wip on 2020 day 7

--- src/2020/day-07/index.ts
+++ src/2020/day-07/index.ts
@@ -1,8 +1,24 @@
-export const getBagsContainingShinyGoldBags = (rulesString: string) => {
-    return null
+type BagContents = { colour: string; amount: number }
+type Bags = {
+    [key: string]: BagContents[]
 }
 
-interface Bag {
-    colour: string
-    contains: Bag[]
+export const getBagsContainingShinyGoldBags = (rulesString: string) => {
+    const rules = rulesString.split('\n')
+    const stuff = rules.reduce<Bags>((bags, rule) => {
+        const [, colour, heldBags] = rule.split(/^(.+?) bags contain /)
+        const dbags = heldBags
+            .split(/[,.]/)
+            .filter((bag) => !!bag)
+            .map<BagContents>((bag) => {
+                const [, amount, colour] = bag.split(/(\d) (.+?) bags?/)
+                return { amount: Number.parseInt(amount), colour }
+            })
+
+        return {
+            ...bags,
+            [colour.trim()]: dbags,
+        }
+    }, {})
+    return 0
 }
