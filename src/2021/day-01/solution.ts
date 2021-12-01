77fcd4aa54c951255f069b3706e270e3b40d7bdc
Atuuh <not-another-one@hotmail.com>
2021-12-01 10:54:52 +0000

    Updated solution.ts

--- src/2021/day-01/solution.ts
+++ src/2021/day-01/solution.ts
@@ -1,11 +1,11 @@
-import { countIncreasingNumbers, countIncreasingNumbersInWindow } from '.'
+import { countIncreasingNumbersInWindow } from '.'
 import { input } from './input'
 
 const getInput = () => input.split('\n').map(Number)
 
-const partOne = () => countIncreasingNumbers(getInput())
+const partOne = () => countIncreasingNumbersInWindow(getInput(), 1)
 
-const partTwo = () => countIncreasingNumbersInWindow(getInput())
+const partTwo = () => countIncreasingNumbersInWindow(getInput(), 3)
 
 console.log('Part One:', partOne())
 console.log('Part Two:', partTwo())
