946e0dce65832bc39503a848b3736f74e42fee88
Atuuh <not-another-one@hotmail.com>
2021-12-02 10:17:42 +0000

    2021 day 3 prep

---
+++ src/2021/day-03/index.test.ts
@@ -0,0 +1,9 @@
+describe('2021 - Day 03', () => {
+    describe('Part One', () => {
+        test.todo('example should work')
+    })
+
+    describe('Part Two', () => {
+        test.todo('example should work')
+    })
+})
---
+++ src/2021/day-03/solution.ts
@@ -0,0 +1,9 @@
+import { parseAndSolve } from '#utils/solve'
+
+const solve = parseAndSolve(2021, 3, (input) => input)
+
+// Part One
+solve(() => 0, 'Part One:')
+
+// Part Two
+solve(() => 0, 'Part Two:')
