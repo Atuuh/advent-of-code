9661aebb18dba007c239c5ef6b53583070ec5aec
Atuuh <not-another-one@hotmail.com>
2022-12-07 11:29:30 +0000

    Generated day 7 files

---
+++ src/2022/day-07/index.test.ts
@@ -0,0 +1,9 @@
+describe('2022 - Day 7', () => {
+    describe('Part One', () => {
+        test.todo('example should work')
+    })
+
+    describe('Part Two', () => {
+        test.todo('example should work')
+    })
+})
