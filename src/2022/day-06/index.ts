f6c4e233d1e1c75e160a98666ed5ea64281c9864
Atuuh <not-another-one@hotmail.com>
2022-12-07 00:58:02 +0000

    2022 day 6 2nd pass

--- src/2022/day-06/index.ts
+++ src/2022/day-06/index.ts
@@ -1,20 +1,13 @@
 export const getFirstMarkerIndex =
     (windowSize: number) =>
     (input: string): number =>
-    1 +
-    input.split('').reduce((foundIndex, value, i, array) => {
-        if (i < windowSize) {
-            return foundIndex
-        }
-        if (foundIndex >= 0) {
-            return foundIndex
-        }
-        const window = array.slice(i - (windowSize - 1), i + 1)
-        if (!hasDuplicates(window)) {
-            return i
-        }
-        return foundIndex
-    }, -1)
+        windowSize +
+        input
+            .split('')
+            .findIndex(
+                (_, index, array) =>
+                    !hasDuplicates(array.slice(index, index + windowSize))
+            )
 
 const hasDuplicates = <T>(array: T[]): boolean =>
     new Set(array).size !== array.length
